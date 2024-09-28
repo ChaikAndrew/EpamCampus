4fc70d7677723eae3ee2edf2340d537a0b7e0651
Andrii <andreychaika87@gmail.com>
2024-09-28 17:42:59 +0200

    REST endpoint
    - top - повертає тільки задану кількість перших записів
    - skip - пропускає задану кількість записів
    - filter - фільтрує записи (формат фільтра OData)
    + вираховувається загальна кількість записів, що проходить фільтр
    - orderby - сортує записи (формат сортування OData)

---
+++ ItMarathon/ItMarathon.Dal/Common/Contracts/IProposalRepository.cs
@@ -0,0 +1,38 @@
+﻿using ItMarathon.Dal.Entities;
+using Microsoft.AspNetCore.OData.Query;
+
+namespace ItMarathon.Dal.Common.Contracts;
+
+/// <summary>
+/// Interface for managing Proposal entities in the repository.
+/// </summary>
+public interface IProposalRepository : IRepositoryBase<Proposal>
+{
+    /// <summary>
+    /// Retrieves list of Proposals entities along with total count.
+    /// </summary>
+    /// <param name="trackChanges">Indicates whether to track changes in the Entity Framework context.</param>
+    /// <param name="queryOptions">OData query options for filtering, sorting, etc.</param>
+    /// <returns>A tuple containing a paginated list of Proposal entities and the total count.</returns>
+    Task<(IEnumerable<Proposal> Proposals, int TotalCount)> GetProposalsAsync(bool trackChanges, ODataQueryOptions queryOptions);
+
+    /// <summary>
+    /// Retrieves a Proposal entity by its ID.
+    /// </summary>
+    /// <param name="proposalId">The ID of the proposal.</param>
+    /// <param name="trackChanges">Indicates whether to track changes in the Entity Framework context.</param>
+    /// <returns>The Proposal entity, or null if not found.</returns>
+    Task<Proposal?> GetProposalAsync(long proposalId, bool trackChanges);
+
+    /// <summary>
+    /// Creates a new Proposal entity.
+    /// </summary>
+    /// <param name="proposal">The Proposal entity.</param>
+    void CreateProposal(Proposal proposal);
+
+    /// <summary>
+    /// Deletes a Proposal entity.
+    /// </summary>
+    /// <param name="proposal">The Proposal entity.</param>
+    void DeleteProposal(Proposal proposal);
+}
---
+++ ItMarathon/ItMarathon.Dal/Repositories/ProposalRepository.cs
@@ -0,0 +1,51 @@
+﻿using ItMarathon.Dal.Common.Contracts;
+using ItMarathon.Dal.Context;
+using ItMarathon.Dal.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.OData.Query;
+
+namespace ItMarathon.Dal.Repositories;
+
+public class ProposalRepository(ApplicationDbContext repositoryContext) :
+    RepositoryBase<Proposal>(repositoryContext), IProposalRepository
+{
+    public async Task<(IEnumerable<Proposal> Proposals, int TotalCount)> GetProposalsAsync(bool trackChanges, ODataQueryOptions queryOptions)
+    {
+        IQueryable<Proposal> query = FindAll(trackChanges);
+
+        // Підрахунок загальної кількості перед застосуванням фільтрів і пагінації
+        var totalCount = await query.CountAsync();
+
+        if (queryOptions != null)
+        {
+            query = (IQueryable<Proposal>)queryOptions.ApplyTo(query);
+        }
+
+        query = query
+            .Include(p => p.AppUser)
+            .Include(p => p.Photos)
+            .Include(p => p.Properties!)
+                .ThenInclude(properties => properties.PropertyDefinition)
+            .Include(p => p.Properties!)
+                .ThenInclude(properties => properties.PredefinedValue)
+                    .ThenInclude(prop => prop!.ParentPropertyValue);
+
+        var proposals = await query.ToListAsync();
+        return (proposals, totalCount);
+    }
+
+    public async Task<Proposal?> GetProposalAsync(long proposalId, bool trackChanges)
+        => await FindByCondition(c => c.Id.Equals(proposalId), trackChanges)
+        .Include(p => p.AppUser)
+        .Include(p => p.Photos)
+        .Include(p => p.Properties!)
+            .ThenInclude(properties => properties.PropertyDefinition)
+        .Include(p => p.Properties!)
+            .ThenInclude(properties => properties.PredefinedValue)
+                .ThenInclude(prop => prop!.ParentPropertyValue)
+        .SingleOrDefaultAsync();
+
+    public void CreateProposal(Proposal proposal) => Create(proposal);
+
+    public void DeleteProposal(Proposal proposal) => Delete(proposal);
+}
